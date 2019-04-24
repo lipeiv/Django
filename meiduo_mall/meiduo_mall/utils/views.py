4b35a92977aa25ac9e3ab07435de434a3ab4cfae
lipeiv <lipeiv@qq.com>
2019-04-24 23:23:53 +0800

    meiduo_mall

---
+++ meiduo_mall/meiduo_mall/utils/views.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.decorators import login_required
+
+
+class LoginRequiredMixin(object):
+
+    @classmethod
+    def as_view(cls, **initkwargs):
+        view = super().as_view()
+        return login_required(view)
