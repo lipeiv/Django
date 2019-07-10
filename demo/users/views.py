7bb9234fa501318b6c4fb01bb9bd700623fb1d50
lipeiv <lipeiv@qq.com>
2019-07-10 22:03:08 +0800

    demo

--- demo/users/views.py
+++ demo/users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse, HttpRequest
+
+
+def index(request):
+
+    return HttpResponse("Hello World!")
