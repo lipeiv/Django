7cd731b569565671f8227a6d47398da22824160a
lipeiv <lipeiv@qq.com>
2019-07-12 18:43:37 +0800

    demo

--- demo/users/views.py
+++ demo/users/views.py
@@ -5,5 +5,9 @@ from django.http import HttpResponse, HttpRequest
 
 
 def index(request):
-
-    return HttpResponse("Hello World!")
+    response = HttpResponse()
+    response.set_cookie('name', 'lipeipei', max_age=3600)
+    cookie = request.COOKIES.get('name')
+    print(cookie)
+    response = HttpResponse("cookie:" + cookie)
+    return response
